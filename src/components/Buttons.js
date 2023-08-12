fbc2331321cd894230a52c81e9401d772aff0dac
Kenneth <kennethrboydjr@gmail.com>
2023-08-12 14:01:56 -0500

    bootleg orbit report complete

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,9 +1,19 @@
-const Buttons = () => {
+import satData from "./satData";
+
+const Buttons = (props) => {
+  let { displaySats, filterByType, setSat } = props;
+
+const buttonOne = displaySats.map((sat, id) => {
   return (
-    <div>
-      <button>Placeholder Button</button>
-      <button>All Orbits</button>
-      </div>
+    <button onClick={() => filterByType(sat)} key={id}>{sat} Orbit</button>
+  )
+})
+
+  return (
+    <div className="Buttonz">
+      <>{ buttonOne }</>
+      <button onClick={() => setSat(satData)}>All Orbits</button>
+    </div>
   );
 };
 
